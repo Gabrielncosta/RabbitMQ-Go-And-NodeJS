e20d1ea586025ebd162ce0b87dbd1149576fb3a2
Gabriel <gabrielncosta208@gmail.com>
2020-12-24 16:10:44 -0300

    feat(producer): send message to queue

--- producer/index.js
+++ producer/index.js
@@ -1,9 +1,27 @@
-require('dotenv/config');
-const amqp = require('amqp-connection-manager')
-
+require('dotenv').config()
+const amqp = require('amqp-connection-manager');
 
 (async () => {
-  const connection = await amqp.connect(process.env.CONNECTION_STRING)
-})()
+  try {
+    const connection = await amqp.connect([process.env.CONNECTION_STRING])
 
-const connection = amqp.connect()
+    const queueName = 'test-queue';
+  
+    const channel = await connection.createChannel({
+      json: true,
+      setup: function(channel) {
+        return channel.assertQueue(queueName, { durable: true })
+      }
+    });
+
+    setInterval(async () => {
+      await channel.sendToQueue(queueName, { hello: 'world '});
+
+      console.log('message sent');
+    }, 5000)
+  } catch (error) {
+    console.log(error)
+  }
+})();
+
+// const connection = amqp.connect()
